ef2e6b5bbfb7bb24a28c64823168cb10dfe3b662
oneandonlycharlie <160505188+oneandonlycharlie@users.noreply.github.com>
2024-08-21 23:42:41 +0800

    Fix: rename functions for readability

--- javascript.js
+++ javascript.js
@@ -1,7 +1,11 @@
-// This part captures all html elements 
+// This part captures all html elements and key varibles
 const buttons = document.querySelector(".buttons")
-const textWindow = document.querySelector("p")
-const resultWindow = document.querySelector(".value")
+const inputHistory = document.querySelector("p")
+const currentInput = document.querySelector(".value")
+
+let operandList = [];
+let currentOperator = '';
+let result = '';
 
 // This part handles all events 
 buttons.addEventListener("click", handleClick)
@@ -11,58 +15,73 @@ buttons.addEventListener("click", handleClick)
 
 function handleClick(e){
     let targetButton = e.target;
-    let currentNumber = result
-    // register user input for calculation later
     switch (targetButton.className){
         case"button-number":
-        // capture numerical value
-            let value = targetButton.textContent;
-            append(value,resultWindow)
+        // capture digit
+            let digit = targetButton.textContent;
+            append(digit,currentInput)
             break;
         case"button-operator":
             // capture operator
-            let operator = targetButton.textContent;
-            currentOperator = operator;
-            // capture number
-            currentNumber = resultWindow.textContent;
-            numberList.push(currentNumber)
-            // display values
-            append(currentNumber,textWindow)
-            append(operator,textWindow)
-            refresh("",resultWindow)
+            currentOperator = targetButton.textContent;
+            // capture operand
+            currnetOperand = currentInput.textContent;
+            operandList.push(currnetOperand)
+            // display inputs
+            append(currnetOperand,inputHistory)
+            append(currentOperator,inputHistory)
+            refresh("",currentInput)
             break;
         case"button-AC":
             allClear("")
             break;
-        case"button-delete":
-        // capture the last input    
-        // find the last input and pop it from both the list and the window
-            deleteLastInput(resultWindow);
+        case"button-delete": 
+        // find the last digit and remove from input
+            deleteLastInput(currentInput);
             break;
         case"button-equal":
-            let equalSign = targetButton.textContent;
-            // capture the second number
-            currentNumber = resultWindow.textContent;
-            numberList.push(currentNumber)
+            // capture the second operand
+            currnetOperand = currentInput.textContent;
+            operandList.push(currnetOperand)
             // only calculate when user presses equal sign
-            result = calculate(currentOperator,numberList);
-            // append(result,textWindow)
-            refresh(result,resultWindow)
-            refresh("",textWindow)
-            reset(numberList,result)
+            result = calculate(currentOperator,operandList);
+            refresh(result,currentInput)
+            refresh("",inputHistory)
+            //reset for next calculation
+            reset(operandList,result)
             break;
         case"button-decimal":
             let decimalPoint = targetButton.textContent;
-            append(decimalPoint,resultWindow)
+            append(decimalPoint,currentInput)
     }
 }
 
 
-// This part handles display and value variables
-let numberList = [];
-let currentOperator = '';
-let result = '';
+// This part handles the logic of calculation
 
+function calculate(operator, operands){
+    // returns the result of calculation or error message
+    // if there are not enough operands, do not calculate
+    if (operands.length >=2 && currentOperator !== ""){
+        switch (operator) {
+            case "+":
+                result = add(operands[0],operands[1]);
+                break;
+            case "-":
+                result = subtract(operands[0],operands[1]);
+                break;
+            case "x":
+                result = multiply(operands[0],operands[1]);
+                break;
+            case "÷":
+                result = divide(operands[0],operands[1]);
+                break;
+        };
+    }
+    return typeof result== "string"? result : result = Math.floor(result*100)/100
+}
+
+// This part handles display functions
 function reset(array,value){
     if (typeof value !== "string") {
         array.splice(0);
@@ -78,44 +97,18 @@ function refresh(text,location){
 }
 
 function allClear(){
-    numberList.splice(0);
+    // clear all inputs and history
+    operandList.splice(0);
     result = '';
-    refresh(result,textWindow);
-    refresh(result,resultWindow);
+    refresh(result,inputHistory);
+    refresh(result,currentInput);
 }
 
 function deleteLastInput(location){
     location.textContent = location.textContent.slice(0,-1)
 }
 
-// This part handles the logic of calculation
-
-function calculate(operator, numbers){
-    // returns the result of calculation or error message
-    // if there is only 1 number, do not calculate
-    // Calculation starts
-    if (numbers.length >=2 && currentOperator !== ""){
-        switch (operator) {
-            case "+":
-                // calculate result
-                result = add(numbers[0],numbers[1]);
-                // display result
-                break;
-            case "-":
-                result = subtract(numbers[0],numbers[1]);
-                break;
-            case "x":
-                result = multiply(numbers[0],numbers[1]);
-                break;
-            case "÷":
-                result = divide(numbers[0],numbers[1]);
-                break;
-        };
-    }
-    return typeof result== "string"? result : result = Math.floor(result*100)/100
-}
-
-// This part handles opetaiton functions
+// This part handles numerical operations
 
 function add(num1,num2){
     return +num1 + +num2
@@ -131,6 +124,7 @@ function multiply(num1,num2){
 
 function divide(num1,num2){
     if (num2=="0"){
+        allClear()
         return "Don't do this.."
     }
     return +num1 / +num2
